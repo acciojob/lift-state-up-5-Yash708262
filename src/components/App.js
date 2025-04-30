7ee41715ae9c00847c7b12d43505352ed0030693
Yash708262 <yash708262@gmail.com>
2025-04-30 23:48:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,6 @@
 import React, { useState } from "react";
+import LoginForm from "./components/LoginForm";
 import "./../styles/App.css";
-
-const LoginForm = ({ isLoggedIn, onLogin }) => {
-  const [username, setUsername] = useState("");
-  const [password, setPassword] = useState("");
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    if (username && password) onLogin();
-  };
-
-  if (isLoggedIn) return <p>You are logged in!</p>;
-
-  return (
-    <form onSubmit={handleSubmit}>
-      <input
-        type="text"
-        value={username}
-        onChange={(e) => setUsername(e.target.value)}
-        placeholder="Username"
-      />
-      <input
-        type="password"
-        value={password}
-        onChange={(e) => setPassword(e.target.value)}
-        placeholder="Password"
-      />
-      <button type="submit">Log In</button>
-    </form>
-  );
-};
 
 const App = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
