66b500d15f991187139eb159e47e7e0dfdfde24b
Yash708262 <yash708262@gmail.com>
2025-04-30 23:48:56 +0530

    Create LoginForm.js

---
+++ src/components/LoginForm.js
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+
+const LoginForm = ({ isLoggedIn, onLogin }) => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (username && password) onLogin();
+  };
+
+  if (isLoggedIn) return <p>You are logged in!</p>;
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={username}
+        onChange={(e) => setUsername(e.target.value)}
+        placeholder="Username"
+      />
+      <input
+        type="password"
+        value={password}
+        onChange={(e) => setPassword(e.target.value)}
+        placeholder="Password"
+      />
+      <button type="submit">Log In</button>
+    </form>
+  );
+};
+
+export default LoginForm;
